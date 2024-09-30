9a85c703dbc9b31c9b89d9d1382ec153fcbfd80b
SergoHop <39134sergei@gmail.com>
2024-09-30 14:42:58 +0300

    сделанно hand + test_hand

---
+++ src/hand.py
@@ -0,0 +1,33 @@
+import typing
+from src.card import Card
+
+class Hand:
+    def __init__(self, cards: list[Card] = None):
+        if cards is None:
+            cards = []
+        self.cards: list[Card] = cards
+
+    def __repr__(self):
+        return self.save()
+    
+    def __eq__(self, other):
+        if isinstance(other, str):
+            other = Hand.load(other)
+        return self.cards == other.cards
+    
+    def save(self) -> str:
+        return ' '.join(card.save() for card in self.cards)
+    
+    @classmethod
+    def load(cls, text: str) -> typing.Self:
+        cards = [Card.load(s) for s in text.split()]
+        return cls(cards=cards)
+
+    def add_card(self, card: Card):
+        self.cards.append(card)
+
+    def remove_card(self, card: Card):
+        self.cards.remove(card)
+
+    
+    
---
+++ test/test_hand.py
@@ -0,0 +1,52 @@
+import random
+
+from src.card import Card
+from src.hand import Hand
+
+cards = [Card(10), Card(20), Card(55)]
+
+def test_init():
+    d = Hand(cards=cards)
+    assert d.cards == cards
+
+def test_repr():
+    d = Hand(cards)
+    d1 = Hand([Card(30)])
+
+    assert d.__repr__() == '10 20 55'
+    assert d1.__repr__() == '30'
+    assert Hand([Card(3)]).__repr__() == '3'
+
+def test_eq():
+    h = Hand(cards)
+    h2 = Hand([Card(30), Card(40), Card(50)])
+    h3 = Hand([Card(40), Card(51)])
+
+    assert h != h2
+    assert h != h3
+
+def test_save():
+    d = Hand(cards=cards)
+    assert d.save() == '10 20 55'
+
+    d = Hand(cards=[])
+    assert d.save() == ''
+
+def test_load():
+    d = Hand.load('10 20 55')
+    expected_deck = Hand(cards)
+    assert d == expected_deck
+
+def test_add_card():
+    h = Hand.load('10 20 55')
+    h.add_card(Card.load('30'))
+    assert repr(h) == '10 20 55 30'
+
+def test_remove_card():
+    h = Hand.load('10 20 55')
+    c = Card.load('20')
+    h.remove_card(c)
+    assert repr(h) == '10 55'
+
+   
+
